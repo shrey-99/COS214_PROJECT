4082be94c2d3e2f74818ec87b9fe9612497c8509
u18202374 <73908386+u18202374@users.noreply.github.com>
2020-11-06 09:08:45 +0000

    Update RaceTrack.h

--- RaceTrack.h
+++ RaceTrack.h
@@ -12,10 +12,17 @@ class RaceTrack
 	private:
 			string shape;
 			string color;
+			string RaceTrackkName;
+			string TyreCompund;
 	public:
 			RaceTrack();
 			void setColor(string);
 			void setShape(string);
+			void setRaceTrackName(string);
+			void setTyreCompund(string);
+			string getRaceTrackkName();
+			string TyreCompund();
+			
 			string getColor();
 			string getShape();
 			virtual string operation()=0;
