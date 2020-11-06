1d68d7d3fb1bf3447ddccd6d0d05e1ca1e89bb77
PeterTA117 <73952456+PeterTA117@users.noreply.github.com>
2020-11-06 23:28:43 +0200

    Update SundayEvent.h

--- SundayEvent.h
+++ SundayEvent.h
@@ -2,13 +2,19 @@
 #define SUNDAYEVENT_H
 
 #include "Event.h"
+#include "Command.h"
 #include <iostream>
 
 class SundayEvent : public Event {
+private:
+	Command* start;
+	Command* end;
 public:
-    SundayEvent(string);
+    SundayEvent(string, Command*, Command*);
     virtual void performEvent();
     void race();
+    void startRace();
+    void endRace();
 };
 
 #endif
