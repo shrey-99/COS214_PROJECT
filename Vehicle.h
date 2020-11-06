6bfea675996c2e8db7f87a26ff8956ea8db01630
PeterTA117 <73952456+PeterTA117@users.noreply.github.com>
2020-11-06 23:21:18 +0200

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -7,14 +7,20 @@ using namespace std;
 
 class Vehicle {
 private:
+    bool isOnState; // vehicle on/off state
     int fuel;
 public:
     Vehicle();
     Vehicle(int);
-    ~Vehicle();
+    bool getState();
+	void setState(bool);
     int getFuel();
     void setFuel(int);
-
+    
+    virtual ~Vehicle();
+    virtual void startEngine() = 0;
+	virtual void stopEngine() = 0;
+	//virtual void notify() = 0; // called in car's startEngine function
 };
 
 #endif
