a7275b7f631b25bc23002315004ddcca6b42fa3e
PeterTA117 <73952456+PeterTA117@users.noreply.github.com>
2020-11-06 23:21:58 +0200

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -19,8 +19,8 @@ public:
     
     virtual ~Vehicle();
     virtual void startEngine() = 0;
-	virtual void stopEngine() = 0;
-	//virtual void notify() = 0; // called in car's startEngine function
+    virtual void stopEngine() = 0;
+    //virtual void notify() = 0; // called in car's startEngine function
 };
 
 #endif
