c6a7e7e77648a16e7ddaeecc9b329e6a9dff2886
u18202374 <73908386+u18202374@users.noreply.github.com>
2020-11-07 10:57:00 +0000

    Update observer.h

--- observer.h
+++ observer.h
@@ -2,12 +2,13 @@
 #define OBSERVER_H
 
 #include <string>
-
+#include "RaceCar.h"
 using namespace std;
 
 class observer {
 public:
     virtual void checkVehicle() = 0;
+    virtual void getcar(RaaceCar*) =0;
 
 };
 
