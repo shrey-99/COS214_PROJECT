6410f53dfca39af35583a9bcdf745522f5eb0d92
PeterTA117 <73952456+PeterTA117@users.noreply.github.com>
2020-11-08 12:57:21 +0200

    Update GrandPrix.cpp

--- GrandPrix.cpp
+++ GrandPrix.cpp
@@ -5,14 +5,19 @@ GrandPrix::GrandPrix()
    cout<<"Created A Grand Prix."<<endl;
 }
 
-GrandPrix:: ~GrandPrix() 
+GrandPrix::~GrandPrix() 
 {
 	cout<<"Grand Prix Deleted."<<endl;
 	delete location;
 }
 
-void GrandPrix ::setLocation(Location* location)
+void GrandPrix::setLocation(Location* location)
 {
     this->location=location;
+}
+
+string* GrandPrix::getFlags()
+{
+    return flags;
 }
 
