e53b4d07eb1ffcc91b8438be4bb2cc738264ca33
u18008390 <73910152+u18008390@users.noreply.github.com>
2020-11-08 22:00:59 +0200

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -24,6 +24,7 @@ public:
     virtual void stopEngine(){};
     virtual void notify(observer*){};
     virtual int calculateTime(){return 0;};
+    virtual int getDamage(){return 0;};
     virtual void increaseSpecs(){};
     virtual void setPosition(int){};
     virtual void updateDamage(){};
